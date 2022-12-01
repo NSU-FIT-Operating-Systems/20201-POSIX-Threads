40ab81b5d3251b42d8f1573fa2f7707d6896dd71
Alexander Yanin <a.yanin@g.nsu.ru>
2022-12-01 16:47:58 +0700

    a.yanin/semaphactory: Handle SIGINT

--- a.yanin/24/src/main.c
+++ a.yanin/24/src/main.c
@@ -1,9 +1,11 @@
 #include <assert.h>
+#include <stdatomic.h>
 #include <string.h>
 #include <time.h>
 
 #include <pthread.h>
 #include <semaphore.h>
+#include <signal.h>
 
 #define LOG_MODE LOG_MODE_SYNC
 #include <common/log/log.h>
@@ -32,6 +34,8 @@ typedef struct {
     part_spec_t *c;
     part_spec_t *d;
 } widget_spec_t;
+
+atomic_bool interrupted = false;
 
 static int uninterruptible_wait(sem_t *sem) {
     int ret = -1;
@@ -67,7 +71,7 @@ static void *run_generic_producer(void *part_spec_opaque) {
     char const *part_name = part_spec->part_name;
     struct timespec interval = part_spec->interval;
 
-    while (true) {
+    while (!interrupted) {
         err_t sleep_err = ERR(wrapper_nanosleep(&interval), NULL);
 
         if (ERR_FAILED(sleep_err)) {
@@ -83,7 +87,7 @@ static void *run_generic_producer(void *part_spec_opaque) {
 static void *run_d_producer(void *d_spec_opaque) {
     d_spec_t *d_spec = d_spec_opaque;
 
-    while (true) {
+    while (!interrupted) {
         retrieve_part(d_spec->a);
         retrieve_part(d_spec->b);
         produce_part(d_spec->d);
@@ -95,7 +99,7 @@ static void *run_d_producer(void *d_spec_opaque) {
 static void *run_widget_producer(void *widget_spec_opaque) {
     widget_spec_t *widget_spec = widget_spec_opaque;
 
-    for (size_t i = 1; ; ++i) {
+    for (size_t i = 1; !interrupted; ++i) {
         retrieve_part(widget_spec->c);
         retrieve_part(widget_spec->d);
         log_printf(LOG_INFO, "Produced a widget! %zu have been made so far", i);
@@ -126,6 +130,11 @@ static void make_part_spec(
         "could not create an output semaphore"));
     ERR_ASSERT(ERR((err_errno_t) sem_init(&spec->available_space, 0, queue_size),
         "could not create an available_space semaphore"));
+}
+
+static void free_part_spec(part_spec_t *spec) {
+    sem_destroy(&spec->output);
+    sem_destroy(&spec->available_space);
 }
 
 int main(int argc, char *[]) {
@@ -167,20 +176,48 @@ int main(int argc, char *[]) {
     };
 
     void *params[5] = { &a, &b, &c, &d_spec, &widget_spec };
+    pthread_t threads[5] = { 0 };
 
-    pthread_attr_t thread_attr;
-    ERR_ASSERT(ERR((err_errno_t) pthread_attr_init(&thread_attr),
-        "could not initialize thread attributes"));
-    pthread_attr_setdetachstate(&thread_attr, PTHREAD_CREATE_DETACHED);
+    sigset_t signal_set;
+    sigemptyset(&signal_set);
+    sigaddset(&signal_set, SIGINT);
+    ERR_ASSERT(ERR((err_errno_t) pthread_sigmask(SIG_BLOCK, &signal_set, NULL),
+        "failed to block signals in the main thread"));
 
-    for (size_t i = 0; i < 5; ++i) {
-        pthread_t id;
-        error = ERR((err_errno_t) pthread_create(&id, &thread_attr, producers[i], params[i]),
+    pthread_t *last_thread_id = &threads[0];
+
+    for (size_t i = 0; i < 5; ++i, ++last_thread_id) {
+        error = ERR((err_errno_t) pthread_create(last_thread_id, NULL, producers[i], params[i]),
             "could not create a producer");
         if (ERR_FAILED(error)) goto pthread_create_fail;
     }
 
+    log_printf(LOG_INFO, "Press ^C to terminate...");
+
+    {
+        int sig;
+        sigwait(&signal_set, &sig);
+    }
+
+    log_printf(LOG_WARN, "Finishing up, please wait...");
+
 pthread_create_fail:
+    interrupted = true;
+
+    for (pthread_t *thread_id = &threads[0]; thread_id != last_thread_id; ++thread_id) {
+        err_t join_error = ERR((err_errno_t) pthread_join(*thread_id, NULL),
+            "could not join a child thread");
+
+        if (ERR_FAILED(join_error)) {
+            err_log_free(LOG_ERR, &join_error);
+        }
+    }
+
+    free_part_spec(&a);
+    free_part_spec(&b);
+    free_part_spec(&c);
+    free_part_spec(&d);
+
     if (ERR_FAILED(error)) {
         err_log_free(LOG_ERR, &error);
 
