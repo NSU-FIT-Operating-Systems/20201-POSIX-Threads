2606291d1e6410cf4af54ee945d4ca7b37a5016b
Alexander Yanin <a.yanin@g.nsu.ru>
2022-12-14 10:43:12 +0700

    a.yanin/common: Reimplement notify_t on top of raw pipe I/O

--- a.yanin/common/loop/include/common/loop/notify.h
+++ a.yanin/common/loop/include/common/loop/notify.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <common/loop/loop.h>
-#include <common/loop/pipe.h>
 
 typedef struct notify notify_t;
 
@@ -13,29 +12,18 @@ typedef error_t *(*notify_cb_t)(loop_t *loop, notify_t *notify);
 // You could think of this as a kind of atomic flag that works with `loop_t`.
 // Posting a notification raises it; once the notification callback is invoked, the flag is reset.
 // Accordingly, posting multiple notifications schedules only one invocation.
-//
-// NOTE: `notify_t` cannot be cast to `handler_t` and must be registered via `notify_register`!
 struct notify {
-    pipe_handler_wr_t *wr;
-    pipe_handler_rd_t *rd;
+    handler_t handler;
 
-    // protected by rd's mtx
+    pthread_mutex_t mtx;
+    int wr_fd;
+
     notify_cb_t on_notified;
-    void *custom_data;
     bool raised;
 };
 
 // Initializes a new instance of `notify_t`.
 error_t *notify_init(notify_t *self);
-
-// Registers `self` in the `loop`.
-//
-// If an error occurs, the resources allocated by `notify_init` are freed.
-// The instance has to be re-initialized afterwards.
-error_t *notify_register(notify_t *self, loop_t *loop);
-
-// Unregisters `self` from the `loop`.
-void notify_unregister(notify_t *self, loop_t *loop);
 
 // Posts a notification on `self`.
 //
@@ -47,14 +35,6 @@ bool notify_post(notify_t *self);
 // Sets the notification callback.
 //
 // If `on_notified` is `NULL` (the default value), no notification will be processed.
+//
+// Must be called from a synchronized context.
 void notify_set_cb(notify_t *self, notify_cb_t on_notified);
-
-// Returns the custom data associated with `self`.
-//
-// The default value is `NULL`.
-void *notify_custom_data(notify_t const *self);
-
-// Associates custom data with `self`.
-//
-// Returns the previous value.
-void *notify_set_custom_data(notify_t *self, void *data);
--- a.yanin/common/loop/src/notify.c
+++ a.yanin/common/loop/src/notify.c
@@ -1,105 +1,117 @@
 #include "common/loop/notify.h"
 
+#include <assert.h>
+
+#include <common/posix/adapter.h>
+#include <common/posix/ipc.h>
+
 #include "util.h"
 
-static error_t *notify_on_read(loop_t *loop, pipe_handler_rd_t *rd, slice_t) {
-    notify_t *self = handler_custom_data((handler_t *) rd);
-    self->raised = false;
+static void notify_free(notify_t *self) {
+    error_t *err = NULL;
+    pthread_mutex_destroy(&self->mtx);
+    err = error_from_posix(wrapper_close(self->wr_fd));
 
-    // FIXME: `self` is passed to `on_notify` while `self->rd` is already locked
-    // this means it's pretty much unusable in the callback
-    // (a possible way to resolve this is to build on top of pipe fds directly, forgoing the pipe
-    // handler approach — I think it's worthwhile...)
-    return self->on_notified(loop, self);
+    if (err) {
+        error_log_free(&err, LOG_WARN, ERROR_VERBOSITY_SOURCE_CHAIN | ERROR_VERBOSITY_BACKTRACE);
+    }
+
+    err = error_from_posix(wrapper_close(handler_fd(&self->handler)));
+
+    if (err) {
+        error_log_free(&err, LOG_WARN, ERROR_VERBOSITY_SOURCE_CHAIN | ERROR_VERBOSITY_BACKTRACE);
+    }
 }
 
+static error_t *notify_process(notify_t *self, loop_t *loop, poll_flags_t flags) {
+    error_t *err = NULL;
+
+    assert_mutex_lock(&self->mtx);
+
+    if (self->raised) {
+        assert(flags & LOOP_READ);
+        char buf = 0;
+        ssize_t read_count = -1;
+
+        err = error_from_posix(wrapper_read(
+            handler_fd(&self->handler),
+            &buf,
+            1,
+            &read_count
+        ));
+        err = error_combine(err, OK_IF(read_count == 1));
+    }
+
+    bool was_raised = self->raised;
+    self->raised = false;
+    assert_mutex_unlock(&self->mtx);
+
+    if (err) goto read_fail;
+
+    if (was_raised) {
+        err = self->on_notified(loop, self);
+
+        if (err) goto cb_fail;
+    }
+
+cb_fail:
+read_fail:
+    return err;
+}
+
+static handler_vtable_t const notify_vtable = {
+    .free = (handler_vtable_free_t) notify_free,
+    .process = (handler_vtable_process_t) notify_process,
+    .on_error = NULL,
+};
+
 error_t *notify_init(notify_t *self) {
-    error_t *err = pipe_handler_new(&self->wr, &self->rd);
+    error_t *err = NULL;
+
+    int rd_fd = -1;
+    int wr_fd = -1;
+    err = error_from_posix(wrapper_pipe(&rd_fd, &wr_fd));
     if (err) goto fail;
 
-    handler_set_custom_data((handler_t *) self->rd, self);
-    handler_set_custom_data((handler_t *) self->wr, self);
+    pthread_mutexattr_t mtx_attr;
+    error_assert(error_wrap("Could not intiialize mutex attributes", error_from_errno(
+        pthread_mutexattr_init(&mtx_attr))));
+    pthread_mutexattr_settype(&mtx_attr, PTHREAD_MUTEX_ERRORCHECK);
+    error_assert(error_wrap("Could not initialize a mutex", error_from_errno(
+        pthread_mutex_init(&self->mtx, &mtx_attr))));
+    pthread_mutexattr_destroy(&mtx_attr);
 
+    handler_init(&self->handler, &notify_vtable, rd_fd);
+    self->wr_fd = wr_fd;
     self->on_notified = (notify_cb_t) { NULL };
-    self->custom_data = NULL;
     self->raised = false;
 
 fail:
     return err;
 }
 
-error_t *notify_register(notify_t *self, loop_t *loop) {
-    error_t *err = NULL;
-
-    bool rd_owned = true;
-    err = loop_register(loop, (handler_t *) self->rd);
-    if (err) goto rd_register_fail;
-
-    rd_owned = false;
-
-    err = loop_register(loop, (handler_t *) self->wr);
-    if (err) goto wr_register_fail;
-
-    return err;
-
-wr_register_fail:
-
-rd_register_fail:
-    if (rd_owned) {
-        handler_free((handler_t *) self->rd);
-    } else {
-        loop_unregister(loop, (handler_t *) self->rd);
-    }
-
-    handler_free((handler_t *) self->wr);
-
-    return err;
-}
-
-void notify_unregister(notify_t *self, loop_t *loop) {
-    loop_unregister(loop, (handler_t *) self->wr);
-    loop_unregister(loop, (handler_t *) self->rd);
-}
-
 bool notify_post(notify_t *self) {
-    static char data[] = { 1 };
-    static slice_t slice = { .base = data, .len = 1};
-
-    handler_lock((handler_t *) self->rd);
+    assert_mutex_lock(&self->mtx);
     bool was_raised = self->raised;
 
     if (!was_raised) {
-        handler_lock((handler_t *) self->wr);
-        error_assert(pipe_write(self->wr, 1, &slice, NULL, NULL));
-        handler_unlock((handler_t *) self->wr);
+        ssize_t written_count = -1;
+        error_assert(error_from_posix(wrapper_write(self->wr_fd, "1", 1, &written_count)));
+        error_assert(OK_IF(written_count == 1));
     }
 
     self->raised = true;
-    handler_unlock((handler_t *) self->rd);
+    assert_mutex_unlock(&self->mtx);
 
     return !was_raised;
 }
 
 void notify_set_cb(notify_t *self, notify_cb_t on_notified) {
-    handler_lock((handler_t *) self->rd);
     self->on_notified = on_notified;
-    pipe_read(self->rd, on_notified ? notify_on_read : NULL, NULL);
-    handler_unlock((handler_t *) self->rd);
-}
 
-void *notify_custom_data(notify_t const *self) {
-    handler_lock((handler_t *) self->rd);
-    void *data = self->custom_data;
-    handler_unlock((handler_t *) self->rd);
-
-    return data;
-}
-
-void *notify_set_custom_data(notify_t *self, void *data) {
-    handler_lock((handler_t *) self->rd);
-    void *prev = self->custom_data;
-    self->custom_data = data;
-    handler_unlock((handler_t *) self->rd);
-
-    return prev;
+    if (on_notified == NULL) {
+        *handler_pending_mask(&self->handler) = 0;
+    } else {
+        *handler_pending_mask(&self->handler) = LOOP_READ;
+    }
 }
