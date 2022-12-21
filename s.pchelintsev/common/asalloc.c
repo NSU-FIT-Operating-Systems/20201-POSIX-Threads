eacdf84724e26bd582f08bfba8858755cdde2074
s.pchelintsev <s.pchelintsev@g.nsu.ru>
2022-12-21 13:27:18 +0700

    exit -> pthread_exit

--- s.pchelintsev/common/asalloc.c
+++ s.pchelintsev/common/asalloc.c
@@ -1,13 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <pthread.h>
+
 // DOWN WITH THE ASSERTS
 
 void* asmalloc(size_t sz) {
 	void* ret = malloc(sz);
 	if (ret == NULL) {
 		printf("[ERROR] Failed to allocate %d bytes, exiting.\n", sz);
-		exit(1);
+		pthread_exit(NULL);
 	}
 	return ret;
 }
@@ -16,7 +18,7 @@ void* ascalloc(size_t amt, size_t sz) {
 	void* ret = calloc(amt, sz);
 	if (ret == NULL) {
 		printf("[ERROR] Failed to allocate %d bytes, exiting.\n", sz);
-		exit(1);
+		pthread_exit(NULL);
 	}
 
 	return ret;
@@ -26,7 +28,7 @@ void* asrealloc(void* ptr, size_t sz) {
 	void* ret = realloc(ptr, sz);
 	if (ret == NULL) {
 		printf("[ERROR] Failed to reallocate %d bytes, exiting.\n", sz);
-		exit(1);
+		pthread_exit(NULL);
 	}
 
 	return ret;
